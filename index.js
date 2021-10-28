08f07b9979f3156e3282cabf7410defe05fb26c5
prithvi <prithvi6@illinois.edu>
2021-10-28 00:51:16 -0500

    final code changes

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 var myScheme = {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "title": "The Top 150 largest Buildings",
     "data": {"url": "https://raw.githubusercontent.com/prithvi-prakash/prithvi-prakash.github.io/master/building_inventory.csv"},
-    "mark": "point",
+    "mark": {
+        "type": "circle",
+        "opacity": 0.5
+        
+    },
+    
+    "height": 400,
+    "width": 600,
+    
+    "transform": [
+    {
+      "window": [{
+        "op": "rank",
+        "as": "rank"
+      }],
+      "sort": [{ "field": "Square Footage", "order": "descending" }]
+    }, {
+      "filter": "datum.rank <= 150"
+    }
+  ],
     "encoding": {
-        "x": {"field": "Year Acquired", "type": "temporal"},
-        "y": {"field": "Year Constructed", "type": "temporal"}
+        "y": {"field": "Agency Name", "sort": "descending"},
+        
+        "x": {"field": "Year Constructed", "type": "temporal"},
+        "tooltip": [
+          {"field": "Location Name", "type": "nominal", "title": "Location"},
+          {"field": "Square Footage", "type": "quantitative", "title": "Building Area"},
+          {"field": "Total Floors", "type": "quantitative", "title": "Number of Floors"},
+          {"field": "City", "type": "nominal", "title": "City"}
+
+        ],
+        "color": {
+          "field":"Bldg Status", 
+          "type": "nominal",
+          "scale": {"range": ["red", "yellow","green"]},
+          "title": "Building Status"
+          },
+        "size": {
+         "field": "Square Footage",
+         "type": "quantitative",
+         "legend": null
+        }
     }
 }
 
